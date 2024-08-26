8221359bb75541cd565ae29b71f669e66248c18f
@mpaez1 <mpaez@ikatechsolutions.com>
2024-08-26 16:33:09 -0500

    add-changes

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -5,7 +5,8 @@ import { MDBCarousel, MDBCarouselItem,  MDBSpinner,
   MDBCardBody,
   MDBCardTitle,
   MDBCardText,
-  MDBCardImage
+  MDBCardImage,
+  MDBBtn
  } from 'mdb-react-ui-kit';
 import '../App.css'; // Asegúrate de importar el archivo CSS si lo estás usando
 
@@ -50,7 +51,7 @@ function Album() {
   return (
     <>
       {carouselItems.length > 0 ? (
-        <MDBCarousel showControls>
+        <MDBCarousel showControls dark>
           {carouselItems.map((item, index) => (
             <MDBCarouselItem itemId={item.id} key={item['id'] || index}>
               <div style={{display: 'flex', justifyContent: 'center'}}>
@@ -64,7 +65,11 @@ function Album() {
           {item['fecha']}
         </MDBCardText>
       <MDBCardTitle>{item['descripcion']}</MDBCardTitle>
-        
+      <br />
+      <MDBBtn href={item['url publica']} target='_blank' className="btn btn-danger">Abrir foto &nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+  <path d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+</svg></MDBBtn>
       </MDBCardBody>
       
     </MDBCard>
