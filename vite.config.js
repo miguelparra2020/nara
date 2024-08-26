4cb0bba77f63f3e8eadd878c2589f08588514ce0
@mpaez1 <mpaez@ikatechsolutions.com>
2024-08-25 22:56:33 -0500

    add-changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nara Paéz Aguirre',
+        short_name: 'Nara Paéz Aguirre',
+        description: 'Nara Paéz Aguirre Album',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192x192-2.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512-2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
